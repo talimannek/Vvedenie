8dc1a2f72e8ef2621dce6a8d6ac5331f16b717aa
talimannek <talimannek@gmail.com>
2024-06-05 17:12:50 +0300

    Add files via upload

---
+++ tali vvedenie.py
@@ -0,0 +1,22 @@
+a = 'Здравствуйте! Очень рада нашей встрече. Было бы неплохо, если бы для выполнения заданий попадали выходные.'
+print(len(a))
+first = 7
+second = 8
+summa = first + second
+diff = first - second
+print(summa)
+print(diff)
+third = 9
+mean = (first + second + third)/3
+print(mean)
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string,', ',first_string)
+a = 10
+b = 11
+c = 12
+f = (a*b)+(a*c)
+d = f**3
+e = d/2
+print(e)
+
